2ee786c6a40109b5cd8131a82527db2246971570
melaniekamm <melanie.a.kammerer@gmail.com>
2020-01-08 12:48:53 -0500

    add master script to run all others together

---
+++ code/RunAllCleaningScripts.R
@@ -0,0 +1,6 @@
+#run all data cleaning scripts
+
+source('./code/1Clean_rawGBIF.R')
+source('./code/2Subset_toMDDE_clean.R')
+source('./code/3Extract_TrapVolumeColor.R')
+source('./code/4CleanAggregate_forArchiving.R')
